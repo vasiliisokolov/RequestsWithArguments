322ef86efe7df4e7e4c3b6690c201641b6b974bc
Vasily Sokolov <vasilysokolov@mail.ru>
2023-08-05 18:53:12 +0300

    Добавьте файлы проекта.

---
+++ RequestsWithArguments.cpp
@@ -0,0 +1,47 @@
+﻿#include "RequestsWithArguments.h"
+
+class Upload
+{
+	cpr::Response r;
+	cpr::Payload p;
+	std::map<std::string, std::string> arguments;
+
+public:
+
+	void setP(std::string key, std::string value)
+	{
+		p.Add(cpr::Pair(key, value));
+	}
+
+	void setArguments(std::string key, std::string value)
+	{
+		arguments.insert(std::make_pair(key, value));
+	}
+
+	void getCPR()
+	{
+		std::cout << "Get!" << std::endl;
+		r = cpr::Get(cpr::Url{"http://httpbin.org/get"});
+	}
+
+	void postCPR()
+	{
+		std::cout << "Post!" << std::endl;
+		r = cpr::Post(cpr::Url{"http://httpbin.org/post"}, p);
+	}
+
+	void out()
+	{
+		std::cout << r.text << std::endl;
+	}
+
+	void run()
+	{
+
+	}
+};
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	
+}
---
+++ RequestsWithArguments.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include <iostream>
+#include <map>
+#include <cpr/cpr.h>
+
