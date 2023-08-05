6507c22e11ad6e3cefaf441a01a2fe1a80ac4915
Vasily Sokolov <vasilysokolov@mail.ru>
2023-08-05 19:36:49 +0300

    error

--- RequestsWithArguments.cpp
+++ RequestsWithArguments.cpp
@@ -8,9 +8,13 @@ class Upload
 
 public:
 
-	void setP(std::string key, std::string value)
+	void setP()
 	{
-		p.Add(cpr::Pair(key, value));
+		std::map<std::string, std::string>::iterator it;
+		for (it = arguments.begin(); it != arguments.end(); it++)
+		{
+			p.Add(cpr::Pair(it->first, it->second));
+		}
 	}
 
 	void setArguments(std::string key, std::string value)
@@ -21,13 +25,29 @@ public:
 	void getCPR()
 	{
 		std::cout << "Get!" << std::endl;
-		r = cpr::Get(cpr::Url{"http://httpbin.org/get"});
+		std::string add ="?";
+		std::map<std::string, std::string>::iterator it;
+		for (it = arguments.begin(); it != arguments.end(); it++)
+		{
+			if (it != arguments.end())
+			{
+				add += it->first + "=" + it->second + "&";
+			}
+			else
+			{
+				add += it->first + "=" + it->second;
+			}
+		}
+		r = cpr::Get(cpr::Url{"http://httpbin.org/get" + add});
+		out();
 	}
 
 	void postCPR()
 	{
+		setP();
 		std::cout << "Post!" << std::endl;
 		r = cpr::Post(cpr::Url{"http://httpbin.org/post"}, p);
+		out();
 	}
 
 	void out()
@@ -39,9 +59,17 @@ public:
 	{
 
 	}
+
+	Upload()
+	{
+		p.GetContent
+
+	};
+		
 };
 int main()
 {
+	Upload upl;
 	std::cout << "Hello CMake." << std::endl;
 	
 }
